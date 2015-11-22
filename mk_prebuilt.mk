8bb055b6b700c5f07855396da1cbeaee667340c8
martincz <martincz@mokeedev.com>
2015-11-23 07:18:34 +0800

    private: improve script
    Change-Id: Idda3f57629054fbf1d40dca30c0cf0adfd5dfd7a

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -17,36 +17,15 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/common/media/location,system/media/location)
 
 # Google apps
+GAPPS_PATH := vendor/private/google
 ifeq ($(MK_CPU_ABI),arm64-v8a)
-STUB := $(shell mkdir -p out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64; \
-    ln -sf /system/lib64/libjni_keyboarddecoder.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_keyboarddecoder.so; \
-    ln -sf /system/lib64/libjni_unbundled_latinimegoogle.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_unbundled_latinimegoogle.so; \
-    mkdir -p out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64; \
-    ln -sf /system/lib64/liben_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/liben_data_bundle.so; \
-    ln -sf /system/lib64/libgnustl_shared.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libgnustl_shared.so; \
-    ln -sf /system/lib64/libhwr.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwr.so; \
-    ln -sf /system/lib64/libhwrword.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrword.so; \
-    ln -sf /system/lib64/libhwrzhmodel.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrzhmodel.so; \
-    ln -sf /system/lib64/libjni_gesture_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_gesture_engine.so; \
-    ln -sf /system/lib64/libjni_hmm_shared_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_hmm_shared_engine.so; \
-    ln -sf /system/lib64/libpinyin_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libpinyin_data_bundle.so;)
+STUB := $(shell $(foreach app, $(shell ls $(GAPPS_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64; \
+    $(foreach lib, $(shell ls $(GAPPS_PATH)/$(app)/$(MK_CPU_ABI)/lib64),ln -sf /system/lib64/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64/$(lib);)))
 else
-STUB := $(shell mkdir -p out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm; \
-    ln -sf /system/lib/libjni_keyboarddecoder.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm/libjni_keyboarddecoder.so; \
-    ln -sf /system/lib/libjni_unbundled_latinimegoogle.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm/libjni_unbundled_latinimegoogle.so; \
-    mkdir -p out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm; \
-    ln -sf /system/lib/liben_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/liben_data_bundle.so; \
-    ln -sf /system/lib/libgnustl_shared.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libgnustl_shared.so; \
-    ln -sf /system/lib/libhwr.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libhwr.so; \
-    ln -sf /system/lib/libhwrword.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libhwrword.so; \
-    ln -sf /system/lib/libhwrzhmodel.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libhwrzhmodel.so; \
-    ln -sf /system/lib/libjni_gesture_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libjni_gesture_engine.so; \
-    ln -sf /system/lib/libjni_hmm_shared_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libjni_hmm_shared_engine.so; \
-    ln -sf /system/lib/libpinyin_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm/libpinyin_data_bundle.so;)
+STUB := $(shell $(foreach app, $(shell ls $(GAPPS_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm; \
+    $(foreach lib, $(shell ls $(GAPPS_PATH)/$(app)/$(MK_CPU_ABI)/lib),ln -sf /system/lib/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm/$(lib);)))
 endif
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.apk,vendor/private/google/GooglePinYin/$(MK_CPU_ABI),system/app/GooglePinYin) \
-    $(call find-copy-subdir-files,*.so,vendor/private/google/GooglePinYin/$(MK_CPU_ABI),system) \
-    $(call find-copy-subdir-files,*.apk,vendor/private/google/GoogleIntl/$(MK_CPU_ABI),system/app/GoogleIntl) \
-    $(call find-copy-subdir-files,*.so,vendor/private/google/GoogleIntl/$(MK_CPU_ABI),system)
+    $(foreach app, $(shell ls $(GAPPS_PATH)),$(call find-copy-subdir-files,*.apk,$(GAPPS_PATH)/$(app)/$(MK_CPU_ABI),system/app/$(app))) \
+    $(foreach app, $(shell ls $(GAPPS_PATH)),$(call find-copy-subdir-files,*.so,$(GAPPS_PATH)/$(app)/$(MK_CPU_ABI),system))
