21f5cf32a628374fdd02a1b96b2de359db2d6e4e
martincz <martincz@mokeedev.com>
2015-11-23 06:11:37 +0800

    private: fix error path

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -19,17 +19,17 @@ PRODUCT_COPY_FILES += \
 # Google apps
 ifeq ($(MK_CPU_ABI),arm64-v8a)
 STUB := $(shell mkdir -p out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64; \
-    ln -sf /system/lib/libjni_keyboarddecoder.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_keyboarddecoder.so; \
-    ln -sf /system/lib/libjni_unbundled_latinimegoogle.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_unbundled_latinimegoogle.so; \
+    ln -sf /system/lib64/libjni_keyboarddecoder.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_keyboarddecoder.so; \
+    ln -sf /system/lib64/libjni_unbundled_latinimegoogle.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm64/libjni_unbundled_latinimegoogle.so; \
     mkdir -p out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64; \
-    ln -sf /system/lib/liben_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/liben_data_bundle.so; \
-    ln -sf /system/lib/libgnustl_shared.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libgnustl_shared.so; \
-    ln -sf /system/lib/libhwr.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwr.so; \
-    ln -sf /system/lib/libhwrword.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrword.so; \
-    ln -sf /system/lib/libhwrzhmodel.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrzhmodel.so; \
-    ln -sf /system/lib/libjni_gesture_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_gesture_engine.so; \
-    ln -sf /system/lib/libjni_hmm_shared_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_hmm_shared_engine.so; \
-    ln -sf /system/lib/libpinyin_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libpinyin_data_bundle.so;)
+    ln -sf /system/lib64/liben_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/liben_data_bundle.so; \
+    ln -sf /system/lib64/libgnustl_shared.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libgnustl_shared.so; \
+    ln -sf /system/lib64/libhwr.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwr.so; \
+    ln -sf /system/lib64/libhwrword.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrword.so; \
+    ln -sf /system/lib64/libhwrzhmodel.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libhwrzhmodel.so; \
+    ln -sf /system/lib64/libjni_gesture_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_gesture_engine.so; \
+    ln -sf /system/lib64/libjni_hmm_shared_engine.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libjni_hmm_shared_engine.so; \
+    ln -sf /system/lib64/libpinyin_data_bundle.so out/target/product/$(MK_BUILD)/system/app/GooglePinYin/lib/arm64/libpinyin_data_bundle.so;)
 else
 STUB := $(shell mkdir -p out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm; \
     ln -sf /system/lib/libjni_keyboarddecoder.so out/target/product/$(MK_BUILD)/system/app/GoogleIntl/lib/arm/libjni_keyboarddecoder.so; \
@@ -47,6 +47,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.apk,vendor/private/google/app/GooglePinYin/$(MK_CPU_ABI),system/app/GooglePinYin) \
-    $(call find-copy-subdir-files,*.apk,vendor/private/google/app/GoogleIntl/$(MK_CPU_ABI),system/app/GoogleIntl) \
     $(call find-copy-subdir-files,*.so,vendor/private/google/app/GooglePinYin/$(MK_CPU_ABI),system) \
+    $(call find-copy-subdir-files,*.apk,vendor/private/google/app/GoogleIntl/$(MK_CPU_ABI),system/app/GoogleIntl) \
     $(call find-copy-subdir-files,*.so,vendor/private/google/app/GoogleIntl/$(MK_CPU_ABI),system)
