0078c465fbf709caded1364c095f7d932ca96e08
martincz <martincz@mokeedev.com>
2015-11-23 22:46:06 +0800

    private: cleanup
    Change-Id: Ifca16bd5fd1be40dbd920cc9b68027d2eb9ed95f

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -17,15 +17,15 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/common/media/location,system/media/location)
 
 # Google apps
-GAPPS_PATH := vendor/private/google
+GAPPS_ROOT_PATH := vendor/private/google
 ifeq ($(MK_CPU_ABI),arm64-v8a)
-STUB := $(shell $(foreach app, $(shell ls $(GAPPS_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64; \
-    $(foreach lib, $(shell ls $(GAPPS_PATH)/$(app)/$(MK_CPU_ABI)/lib64),ln -sf /system/lib64/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64/$(lib);)))
+$(shell $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64; \
+    $(foreach lib, $(shell test -d $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib64 && ls $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib64),ln -sf /system/lib64/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm64/$(lib);)))
 else
-STUB := $(shell $(foreach app, $(shell ls $(GAPPS_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm; \
-    $(foreach lib, $(shell ls $(GAPPS_PATH)/$(app)/$(MK_CPU_ABI)/lib),ln -sf /system/lib/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm/$(lib);)))
+$(shell $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm; \
+    $(foreach lib, $(shell test -d $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib && ls $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib),ln -sf /system/lib/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm/$(lib);)))
 endif
 
 PRODUCT_COPY_FILES += \
-    $(foreach app, $(shell ls $(GAPPS_PATH)),$(call find-copy-subdir-files,*.apk,$(GAPPS_PATH)/$(app)/$(MK_CPU_ABI),system/app/$(app))) \
-    $(foreach app, $(shell ls $(GAPPS_PATH)),$(call find-copy-subdir-files,*.so,$(GAPPS_PATH)/$(app)/$(MK_CPU_ABI),system))
+    $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),$(call find-copy-subdir-files,*.apk,$(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI),system/app/$(app))) \
+    $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),$(call find-copy-subdir-files,*.so,$(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI),system))
