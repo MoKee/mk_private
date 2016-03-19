0b26ace6a01d71e8f930bfb4df03d390d15eac72
martincz <martincz@mokeedev.com>
2016-03-19 12:09:42 +0800

    v4a audio effect hook (3/4)
    Change-Id: I975776f5413f8c7badd68e90e511f14aa1584021

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -16,7 +16,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.so,vendor/private/lib/$(MK_CPU_ABI),system/lib)
 endif
 
-# Offline Phone Location Database
+# Offline phone location database
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/common/media/location,system/media/location)
 
@@ -29,7 +29,13 @@ else
 $(shell $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),mkdir -p out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm; \
     $(foreach lib, $(shell test -d $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib && ls $(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI)/lib),ln -sf /system/lib/$(lib) out/target/product/$(MK_BUILD)/system/app/$(app)/lib/arm/$(lib);)))
 endif
-
 PRODUCT_COPY_FILES += \
     $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),$(call find-copy-subdir-files,*.apk,$(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI),system/app/$(app))) \
     $(foreach app, $(shell ls $(GAPPS_ROOT_PATH)),$(call find-copy-subdir-files,*.so,$(GAPPS_ROOT_PATH)/$(app)/$(MK_CPU_ABI),system))
+
+# V4a apps
+ifneq ($(filter armeabi armeabi-v7a,$(MK_CPU_ABI)),)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/private/viper/app,system/app) \
+    $(call find-copy-subdir-files,*.so,vendor/private/viper/lib/armeabi-v7a/soundfx,system/lib/soundfx)
+endif
