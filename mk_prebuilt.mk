801093bf6feef821f7b1eca161e9028e22939040
martincz <martincz@mokeedev.com>
2016-04-01 20:32:33 +0800

    private: exclude low system space devices
    Change-Id: Idc704edc9d8b57c5c59b4d55c56aafcd1352d75f

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -20,20 +20,37 @@ endif
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/common/media/location,system/media/location)
 
-# Google apps
+# Google and Browser apps
 ifeq ($(filter armeabi armeabi-v7a arm64-v8a,$(MK_CPU_ABI)),)
 PRODUCT_PACKAGES += \
     Browser \
     LatinIME
+else
+ifneq ($(filter i9100, $(MK_BUILD)),)
+PRODUCT_PACKAGES += \
+    Browser \
+    GoogleIntl \
+    GooglePinYin
 else
 PRODUCT_PACKAGES += \
     Chromium \
     GoogleIntl \
     GooglePinYin
 endif
+endif
 
 # V4a modules
-ifneq ($(MK_CPU_ABI),arm64-v8a)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.so,vendor/private/viper/lib/$(MK_CPU_ABI),system/lib)
+ifeq ($(filter armeabi armeabi-v7a x86,$(MK_CPU_ABI)),)
+PRODUCT_PACKAGES += \
+    AudioFX
+else
+ifneq ($(filter shamu victara victara_retcn, $(MK_BUILD)),)
+PRODUCT_PACKAGES += \
+    AudioFX
+else
+PRODUCT_PACKAGES += \
+    ViPER4Android
+endif
 endif
