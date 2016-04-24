134de844f218882d218011670138a2cf0d30ca1d
martincz <martincz@mokeedev.com>
2016-04-25 01:12:44 +0800

    private: cleanup
    Change-Id: I42feeb8d283da2e58d3905b7646fbff849ab3da3

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -1,24 +1,24 @@
 # Use all private apps
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/private/priv-app/,system/priv-app)
+PRODUCT_PACKAGES += \
+    MoKeePay
 
 # Use all private binaries
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/private/bin/$(MK_CPU_ABI),system/bin)
+    $(call find-copy-subdir-files,*,vendor/private/mokee/bin/$(MK_CPU_ABI),system/bin)
 
 # Use all private libraries
 ifeq ($(MK_CPU_ABI),arm64-v8a)
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/private/lib/$(MK_CPU_ABI),system/lib64) \
-    $(call find-copy-subdir-files,*.so,vendor/private/lib/armeabi-v7a,system/lib)
+    $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/$(MK_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/armeabi-v7a,system/lib)
 else
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/private/lib/$(MK_CPU_ABI),system/lib)
+    $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/$(MK_CPU_ABI),system/lib)
 endif
 
 # Offline phone location database
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/common/media/location,system/media/location)
+    $(call find-copy-subdir-files,mokee-phonelocation.dat,vendor/private/mokee/common/media/location,system/media/location)
 
 # Google and Browser apps
 ifeq ($(filter armeabi armeabi-v7a arm64-v8a,$(MK_CPU_ABI)),)
