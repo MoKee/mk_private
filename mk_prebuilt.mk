702db31fee6bf99b0cad6bcbc7f9b504d80b2e75
martincz <martincz@mokeedev.com>
2016-06-29 13:51:27 +0800

    Disable dex-preopt dior
    Change-Id: Ia4dd1f136e05f83b56ed8f1c1e5045214bf17f70

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -57,6 +57,6 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Disable dex-preopt of some devices to save space, if requested.
-ifneq ($(filter armani cancro i9100 umts_spyder spyder maserati targa, $(MK_BUILD)),)
+ifneq ($(filter armani cancro dior i9100 umts_spyder spyder maserati targa, $(MK_BUILD)),)
 WITH_DEXPREOPT := false
 endif
