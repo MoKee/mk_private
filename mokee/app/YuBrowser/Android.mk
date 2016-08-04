03639c45ea32e00876212d748b25f231cebac0b5
martincz <martincz@mokeedev.com>
2016-08-05 00:35:00 +0800

    update YuBrowser
    Change-Id: Ib18efa8dc448bbc15ca976e4f33b96ea7b8b8627

--- mokee/app/YuBrowser/Android.mk
+++ mokee/app/YuBrowser/Android.mk
@@ -29,20 +29,5 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 
 LOCAL_MULTILIB := 32
-LOCAL_PREBUILT_JNI_LIBS := \
-	lib/arm/libc++_shared.so \
-	lib/arm/libgiga_client.so \
-	lib/arm/libicuuc.cr.so \
-	lib/arm/libicui18n.cr.so \
-	lib/arm/libswecore.so \
-	lib/arm/libsweadrenoext_23_plugin.so \
-	lib/arm/libswenetxt_plugin.so \
-	lib/arm/libswev8.so \
-	lib/arm/libsweskia.so \
-	lib/arm/libsta.so \
-	lib/arm/libswe.so \
-	lib/arm/libsweadrenoext_plugin.so \
-	lib/arm/libsweadrenoext_22_plugin.so \
-	lib/arm/libswewebrefiner.so \
 
 include $(BUILD_PREBUILT)
