08bf605c90babd17e6b343e99e90a0153cdeee7a
martincz <martincz@mokeedev.com>
2016-09-20 01:37:11 +0800

    private: disable skip dexopt list
    Change-Id: I8ad43e0c03e0a02635f03ffc3fc0b21d9fab7f6a

--- mk_prebuilt.mk
+++ mk_prebuilt.mk
@@ -57,6 +57,6 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Disable dex-preopt of some devices to save space, if requested.
-ifneq ($(filter cancro i9100 umts_spyder spyder maserati targa, $(MK_BUILD)),)
-WITH_DEXPREOPT := false
-endif
+# ifneq ($(filter cancro i9100 umts_spyder spyder maserati targa, $(MK_BUILD)),)
+# WITH_DEXPREOPT := false
+# endif
