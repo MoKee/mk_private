fefb20f4f5c568cc44b42bdf4482ab1b1ea94cba
martincz <martincz@mokeedev.com>
2017-07-19 22:02:20 +0800

    private: add x86 prop
    Change-Id: I7c5f967efe78f3dbc8a24e6d119e18fcd1f645be

--- xposed/Android.mk
+++ xposed/Android.mk
@@ -37,7 +37,9 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)
-ifeq ($(MK_CPU_ABI),arm64-v8a)
+ifeq ($(MK_CPU_ABI),x86)
+LOCAL_SRC_FILES := prop/x86/xposed.prop
+else ifeq ($(MK_CPU_ABI),arm64-v8a)
 LOCAL_SRC_FILES := prop/arm64/xposed.prop
 else
 LOCAL_SRC_FILES := prop/arm/xposed.prop
