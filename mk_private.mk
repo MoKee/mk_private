a220ce76e107bbd2dea0727d849a8d80fee1558a
martincz <martincz@mokeedev.com>
2017-08-09 23:10:55 +0800

    private: disable dexopt for h850
    Change-Id: I35735b1a60c1dff3f049e01c7626e2acb11cbbdf

--- mk_private.mk
+++ mk_private.mk
@@ -53,7 +53,7 @@ WITH_DEXPREOPT := false
 endif
 
 # Disable dex-preopt of some devices to fix compile.
-ifneq ($(filter h811 h815 h830,$(MK_BUILD)),)
+ifneq ($(filter h811 h815 h830 h850,$(MK_BUILD)),)
 WITH_DEXPREOPT := false
 endif
 
