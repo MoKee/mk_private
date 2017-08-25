3faf1fac29cd12e1b926067f55db5338b8e3e817
martincz <martincz@mokeedev.com>
2017-08-26 07:50:52 +0800

    private: move source effects apps to common (1/2)
    Change-Id: I3fc0a2c4a85835493f7205bfe6051446e0ae6afc

--- mk_private.mk
+++ mk_private.mk
@@ -57,10 +57,6 @@ ifneq ($(filter h811 h815 h830 h850,$(MK_BUILD)),)
 WITH_DEXPREOPT := false
 endif
 
-# Default sound effects app
-PRODUCT_PACKAGES += \
-    AudioFX
-
 # Built-in Xposed
 PRODUCT_PACKAGES += \
     libxposed_art \
@@ -75,19 +71,3 @@ else
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/$(MK_CPU_ABI),system/lib)
 endif
-
-# V4a modules
-# ifeq ($(filter armeabi armeabi-v7a x86,$(MK_CPU_ABI)),)
-# PRODUCT_PACKAGES += \
-#     AudioFX
-# else
-# ifneq ($(filter shamu victara victara_retcn, $(MK_BUILD)),)
-# PRODUCT_PACKAGES += \
-#     AudioFX
-# else
-# PRODUCT_PACKAGES += \
-#     ViPER4Android
-# endif
-# PRODUCT_COPY_FILES += \
-#     $(call find-copy-subdir-files,*.so,vendor/private/viper/lib/$(MK_CPU_ABI),system/lib)
-# endif
