ba973bce61782cc33c161fffcd78726b9dfe74df
martincz <martincz@mokeedev.com>
2017-08-26 07:53:49 +0800

    private: cleanup duplicate script
    Change-Id: Iff26186ce11883f49296d91748138b206928cc2d

--- mk_private.mk
+++ mk_private.mk
@@ -63,11 +63,3 @@ endif
 #     XposedBridge \
 #     XposedInstaller \
 #     xposed.prop
-
-ifeq ($(MK_CPU_ABI),arm64-v8a)
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/$(MK_CPU_ABI),system/lib64)
-else
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.so,vendor/private/mokee/lib/$(MK_CPU_ABI),system/lib)
-endif
